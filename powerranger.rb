c9791cce41df6abd7a133a2853289bad4af0d7be
Nora Neyland <noraleeneyland@gmail.com>
2014-11-14 12:50:51 -0600

    add power ranger subclass

--- powerranger.rb
+++ powerranger.rb
@@ -1,7 +1,52 @@
 class Person
+	attr_accessor :name, :caffeine_level
+
+	def initialize(name)
+		@name = name
+		@caffeine_level = 0
+	end
+	
+	def drink_coffee(cups)
+		@caffeine_level += cups
+		puts "You drank #{cups} cups of coffee!  You now have #{caffeine_level} caffeine!"
+	end
+
+	def run
+		puts "You ran away?  I thought you were a Power Ranger!"
+	end
+
+	def scream
+		puts "AHHHHHH REAL MONSTERS!"
+	end
 end
 
-class PowerRanger
+class PowerRanger < Person
+	attr_accessor :name, :strength, :color
+
+	def initialize(name, color)
+		@name = name
+		@strenth = 0
+		@color = color
+		puts "It's morphin time! #{color.capitalize} Ranger!"
+	end
+
+	def push_ups(reps)
+		@strength += reps
+		puts "Your strength is now #{reps} higher, a total of #{strength}!"
+	end
+
+	def punch(being_punched)
+		if @name.strength > 5
+			@caffeine_level -= 10
+			puts "#{being_punched} was somersaulted in to the air by your punch! Your caffeine level is now at #{caffeine_level}."
+		else
+			@caffeine_level -= 5
+			puts "#{being_punched} screamed and ran away! Your caffeine level is now at #{caffeine_level}."
+		end
+	end
+
+	def rest
+	end
 end
 
 class EvilNinja
