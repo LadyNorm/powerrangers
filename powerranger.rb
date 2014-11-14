36bce9ca42a294995d8f99763def47b1e888e103
Nora Neyland <noraleeneyland@gmail.com>
2014-11-14 13:16:13 -0600

    Moved punch to person class, added evilninja subclass

--- powerranger.rb
+++ powerranger.rb
@@ -18,6 +18,16 @@ class Person
 	def scream
 		puts "AHHHHHH REAL MONSTERS!"
 	end
+
+	def punch(being_punched)
+		if @name.strength > 5
+			@caffeine_level -= 10
+			puts "#{being_punched} was somersaulted in to the air by your punch! Your caffeine level is now at #{caffeine_level}."
+		else
+			@caffeine_level -= 5
+			puts "#{being_punched} screamed and ran away! Your caffeine level is now at #{caffeine_level}."
+		end
+	end
 end
 
 class PowerRanger < Person
@@ -35,19 +45,31 @@ class PowerRanger < Person
 		puts "Your strength is now #{reps} higher, a total of #{strength}!"
 	end
 
-	def punch(being_punched)
-		if @name.strength > 5
-			@caffeine_level -= 10
-			puts "#{being_punched} was somersaulted in to the air by your punch! Your caffeine level is now at #{caffeine_level}."
-		else
-			@caffeine_level -= 5
-			puts "#{being_punched} screamed and ran away! Your caffeine level is now at #{caffeine_level}."
-		end
+	def rest(hours)
+		@strength += (hours/2.0)
+		puts "After resting for #{hours} hours, your strength is now at #{strength}!"
 	end
 
-	def rest
+	def use_megazord
+		if @strength > 5000
+			puts "You pull out your megazord, the Evil Ninja sees that he will never defeat you, soils his pants, and offers you his first and second born children."
+		else
+			@strength -= 10
+			puts "You fool, you must have a strength level of at least 5000 to use the megazord.  Lose 10 strength for even trying.  Your strength is now at #{strength}."
 	end
 end
 
-class EvilNinja
+class EvilNinja < Person
+	attr_accessor :strength, :evilness
+
+	def initialize(name)
+		@name = name
+		@strength = 0
+		@evilness = 0
+	end
+
+	def cause_mayhem(victim)
+		victim.caffeine_level = 0
+		puts "The Evil Ninja has struck again!! #{victim.name} now has NO CAFFEINE!  Oh the humanity....."
+	end
 end
